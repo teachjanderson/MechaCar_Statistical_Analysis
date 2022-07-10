89b5fdedd731e8a41c308315d0139a8f2a7a8b60
teachjanderson <teachjanderson@gmail.com>
2022-07-10 12:14:20 -0500

    Updated with lot analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,6 @@
 # Use the library() function to load the dplyr package.
 library(dplyr)
+library(ggplot2)
 
 # Import and read CSV of car data
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
@@ -25,3 +26,15 @@ Suspension_Total <- SuspensionCoil %>% summarize(Mean_Suspension=mean(PSI), Medi
 
 # Create a summary DataFrame grouping by lot using group_by and summarize for each lot
 Manufacture_Total <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_Suspension=mean(PSI), Median_Supsension=median(PSI), Variance_Supsension=var(PSI), SD_Supsension=sd(PSI), SusCoils=n(), .groups = 'keep')
+
+#Create a boxplot of the suspension totals
+Suspension_plt <- ggplot(SuspensionCoil,aes(y=PSI)) 
+Suspension_plt + geom_boxplot() 
+
+# T.Test to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch. 
+t.test(SuspensionCoil$PSI,mu=1500)
+
+# Separate T.Tests to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+MFLot1 <- t.test(subset(SuspensionCoil, Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+MFLot2 <- t.test(subset(SuspensionCoil, Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+MFLot3 <- t.test(subset(SuspensionCoil, Manufacturing_Lot=="Lot3")$PSI,mu=1500)
