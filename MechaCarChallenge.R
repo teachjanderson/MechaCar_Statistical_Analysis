1efd5e6fe17082106b6d42ac125e6bf836a1ff5f
teachjanderson <teachjanderson@gmail.com>
2022-07-10 11:31:49 -0500

    Updated suspension coil DataFrame

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,16 @@ MechaLin <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cl
 
 # Create a summary to determine p-value and r-squared for linear model
 MechaSum <- summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar))
+
+# Suspension Coil Analysis
+
+# Import and read CSV of suspension coil data
+
+# Import and read CSV of car data
+SuspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create a total summary data frame using summarize function of suspension data
+Suspension_Total <- SuspensionCoil %>% summarize(Mean_Suspension=mean(PSI), Median_Supsension=median(PSI), Variance_Supsension=var(PSI), SD_Supsension=sd(PSI), SusCoils=n(), .groups = 'keep')
+
+# Create a summary DataFrame grouping by lot using group_by and summarize for each lot
+Manufacture_Total <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_Suspension=mean(PSI), Median_Supsension=median(PSI), Variance_Supsension=var(PSI), SD_Supsension=sd(PSI), SusCoils=n(), .groups = 'keep')
