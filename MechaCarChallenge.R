86d04322504e85f90e197e845a5619da06c8d3ab
teachjanderson <teachjanderson@gmail.com>
2022-07-10 15:27:01 -0500

    Updated files and structure

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,7 +10,8 @@ MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 class(MechaCar)
 
 #Create linear regression of all DataFrame variables
-MechaLin <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar) 
+MechaLin <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
 
 # Create a summary to determine p-value and r-squared for linear model
 MechaSum <- summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar))
